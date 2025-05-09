72f6378e176b389085c6f94508eab105a2d41439
KamaelLeRandom <KamaelLeRandom@gmail.com>
2025-05-09 18:49:10 +0200

    feat: implementation of the first 'NavBar' component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  standalone: true,
+  imports: [NgbTooltipModule, RouterLink, RouterLinkActive, NgIf],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  isConnected: boolean;
 
+  constructor() {
+    this.isConnected = true;
+  }
 }
